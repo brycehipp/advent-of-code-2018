927a7542fee669a50b035de232906ed6b611dcf9
Bryce Hipp <bhipp@qetechnology.com>
2018-12-02 12:01:39 -0600

    Remove unneeded util

--- src/2018/utils.ts
+++
@@ -1,9 +0,0 @@
-import { readFile as _readFile } from 'fs'
-import { promisify } from 'util'
-
-// Convert fs.readFile into Promise version of same
-export const readFile = promisify(_readFile)
-
-export default {
-  readFile,
-}
